3af88645c9995b2abf63b7be14ac90c151963d9d
Barion <ametrin.studios@gmail.com>
2023-07-31 10:19:05 +0200

    init

---
+++ DirectoryInfoJsonConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Ametrin.Serialization;
+public sealed class DirectoryInfoJsonConverter : JsonConverter<DirectoryInfo> {
+    public override DirectoryInfo? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+        if(reader.TokenType == JsonTokenType.Null) return null;
+        var value = reader.GetString();
+        return string.IsNullOrWhiteSpace(value) ? null : new(value);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DirectoryInfo value, JsonSerializerOptions options) {
+        writer.WriteStringValue(value.FullName);
+    }
+}
---
+++ JsonExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+
+namespace Ametrin.Serialization;
+public static class JsonExtensions {
+    public static readonly JsonSerializerOptions DefaultOptions = new() { WriteIndented = true, IncludeFields = true };
+    static JsonExtensions() {
+        DefaultOptions.Converters.Add(new DirectoryInfoJsonConverter());
+        DefaultOptions.Converters.Add(new FileInfoJsonConverter());
+    }
+
+    public static string ConvertToJson<T>(this T data, JsonSerializerOptions? options = null) => JsonSerializer.Serialize(data, options ?? DefaultOptions);
+    public static JsonElement ConvertToJsonElement<T>(this T data, JsonSerializerOptions? options = null) => JsonSerializer.SerializeToElement(data, data!.GetType(), options ?? DefaultOptions);
+
+    public static Task<string> ConvertToJsonAsync<T>(this T data, JsonSerializerOptions? options = null) => Task.Run(()=> data.ConvertToJson(options));
+    public static Task<JsonElement> ConvertToJsonElementAsync<T>(this T data, JsonSerializerOptions? options = null) => Task.Run(()=> data.ConvertToJsonElement(options));
+    
+    
+    public static void WriteToJsonFile<T>(this T data, string path, JsonSerializerOptions? options = null) {
+        File.WriteAllText(path, data.ConvertToJson(options ?? DefaultOptions));
+    }
+
+    public static async Task WriteToJsonFileAsync<T>(this T data, string path, JsonSerializerOptions? options = null) {
+        using var stream = File.Create(path);
+        await JsonSerializer.SerializeAsync(stream, data, options ?? DefaultOptions);
+        await stream.DisposeAsync();
+    }
+
+    public static T? ReadFromJsonFile<T>(string path) {
+        string json = File.ReadAllText(path);
+        return JsonSerializer.Deserialize<T>(json);
+    }
+
+    public static async Task<T?> ReadFromJsonFileAsync<T>(string path) {
+        using FileStream stream = File.OpenRead(path);
+        return await JsonSerializer.DeserializeAsync<T>(stream);
+    }
+
+    public static void WriteToJsonFile<T>(this T data, FileInfo fileInfo, JsonSerializerOptions? options = null) => data.WriteToJsonFile(fileInfo.FullName, options);
+    public static Task WriteToJsonFileAsync<T>(this T data, FileInfo fileInfo, JsonSerializerOptions? options = null) => data.WriteToJsonFileAsync(fileInfo.FullName, options);
+    public static T? ReadFromJsonFile<T>(FileInfo fileInfo) => ReadFromJsonFile<T>(fileInfo.FullName);
+    public static Task<T?> ReadFromJsonFileAsync<T>(FileInfo fileInfo) => ReadFromJsonFileAsync<T>(fileInfo.FullName);
+}
